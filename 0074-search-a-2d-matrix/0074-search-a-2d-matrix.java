f2d92174798501ea656a67f47e01476b06328998
324103382005-cmd <324103382005@gvpce.ac.in>
2026-08-09 11:31:03 +0530

    Time: 0 ms (100%), Space: 44.2 MB (8.95%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) {
+            return false;
+        }
+
+        int n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (target == matrix[mid / n][mid % n]) {
+                return true;
+            }
+
+            if (target < matrix[mid / n][mid % n]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
